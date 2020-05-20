df116a15f7710238a51e836bcb4cfb78b13e0fa6
ShadeUser <thelexxus@rambler.ru>
2020-05-20 11:37:55 +0500

    New COMMIT!!!!!!!!!!!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    //commit!!!!!!!!
+
+
+        //Всем приветы!!!!!!!!!!
+
+
+        ..// TOFLLGKFLGKFLGFLGLFJLFJHFH
+
+        //dfgdfgdfgdgdgd
     }
 }
